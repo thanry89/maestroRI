a03223374451b8cf824f74f8a3538ac06b422f89
JonathanZ <thanry89@gmail.com>
2025-04-17 20:03:05 -0500

    Incluir Pagina de Login

--- maestro.py
+++ maestro.py
@@ -1,3 +1,26 @@
 import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
 
-st.text('Archivo Maestro')
+with open('data/config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['expiry_days']
+)
+
+try:
+    authenticator.login()
+except Exception as e:
+    st.error(e)
+
+if st.session_state.get('authentication_status'):
+    authenticator.logout()
+    st.write(f'Welcome *{st.session_state.get("name")}*')
+    st.title('Archivo Maestro')
+elif st.session_state.get('authentication_status') is False:
+    st.error('Username/password is incorrect')
+elif st.session_state.get('authentication_status') is None:
+    st.warning('Please enter your username and password')
