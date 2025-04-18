2610147e3263966dcd4d29be2d200865c3561c34
JonathanZ <thanry89@gmail.com>
2025-04-17 22:44:21 -0500

    Primera prueba con Archivo

--- maestro.py
+++ maestro.py
@@ -2,6 +2,7 @@ import streamlit as st
 import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
+import pandas as pd
 
 with open('data/config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
@@ -20,6 +21,19 @@ if st.session_state.get('authentication_status'):
     authenticator.logout()
     st.write(f'Welcome *{st.session_state.get("name")}*')
     st.title('Archivo Maestro')
+
+    # Load data
+    data = pd.read_excel('data/maestroRI.xlsx', dtype=str)
+
+    site = st.selectbox(
+        'Seleccionar Sitio',
+        data['Nombre del Sitio'].sort_values().tolist(),
+        index=None,
+        placeholder='Seleccionar Sitio...'
+    )
+
+    st.dataframe(data[data['Nombre del Sitio'] == site], hide_index=True, height=90, width=3000)
+
 elif st.session_state.get('authentication_status') is False:
     st.error('Username/password is incorrect')
 elif st.session_state.get('authentication_status') is None:
